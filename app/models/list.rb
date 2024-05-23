e71aa198fbf1ed851c9864dde7b52974de1639aa
Benoit LEDEUIL <benoitledeuil@gmail.com>
2024-05-23 11:37:07 +0200

    created models

---
+++ app/models/list.rb
@@ -0,0 +1,7 @@
+class List < ApplicationRecord
+  has_many :bookmarks, :dependent => :delete_all
+  has_many :movies, through: :bookmarks
+
+  validates :name, presence: true, uniqueness: true
+
+end
