9f5ef28a9c28364c2cd84cbc4efdde71d476f71f
Benoit LEDEUIL <benoitledeuil@gmail.com>
2024-05-24 16:40:27 +0200

    link to cloudinary

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,3 +1,4 @@
+
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
@@ -34,7 +35,7 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = :cloudinary
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
